7b3f37535a4c51c193a46d8d1d8ff3aa514c362d
Omar Shabaan <omarshabaan.dev@gmail.com>
2025-01-18 23:48:48 +0200

    :sparkles: add statistics field to track game statistics

--- RockPaperScissorsGame/src/main/kotlin/com/hadsol/game/Game.kt
+++ RockPaperScissorsGame/src/main/kotlin/com/hadsol/game/Game.kt
@@ -1,6 +1,8 @@
 package com.hadsol.game
 
 class Game(private val playerA: Player, private val playerB: Player) {
+    private var statistics = GameStatistics()
+
     fun playRound(): Result {
         val choiceA = playerA.choose()
         val choiceB = playerB.choose()
@@ -9,6 +11,16 @@ class Game(private val playerA: Player, private val playerB: Player) {
             choiceA == choiceB -> Result.DRAW
             choiceA.beats(choiceB) -> Result.WIN
             else -> Result.LOSE
+        }.also { updateStatistics(it) }
+    }
+
+    private fun updateStatistics(result: Result) {
+        statistics = when (result) {
+            Result.WIN -> statistics.copy(firstPlayerWins = statistics.firstPlayerWins + 1)
+            Result.LOSE -> statistics.copy(secondPlayerWins = statistics.secondPlayerWins + 1)
+            Result.DRAW -> statistics.copy(draws = statistics.draws + 1)
         }
     }
+
+    fun getStatistics(): String = statistics.format(firstPlayerName = playerA.getName(), secondPlayerName = playerB.getName())
 }

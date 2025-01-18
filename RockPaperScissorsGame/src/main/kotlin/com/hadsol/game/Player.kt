1d40327da2f16fff340d7bcbd51b205bbcf78c3e
Omar Shabaan <omarshabaan.dev@gmail.com>
2025-01-19 00:45:36 +0200

    :pencil2: Change players default names

--- RockPaperScissorsGame/src/main/kotlin/com/hadsol/game/Player.kt
+++ RockPaperScissorsGame/src/main/kotlin/com/hadsol/game/Player.kt
@@ -5,12 +5,12 @@ interface Player {
     fun getName(): String
 }
 
-class RockPlayer(private val name: String = "Player A") : Player {
+class RockPlayer(private val name: String = "Player B") : Player {
     override fun choose(): Choice = Choice.ROCK
     override fun getName(): String = name
 }
 
-class RandomPlayer(private val name: String = "Player B") : Player {
+class RandomPlayer(private val name: String = "Player A") : Player {
     override fun choose(): Choice = Choice.values().random()
     override fun getName(): String = name
 }
